5b74479f57b19cb61ecb607a2533005d311543c0
Ervin Yong <elrsple@hotmail.com>
2023-08-18 15:22:16 +0800

    Newest Update

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./App.css";
+import { Login } from "./Login";
+// import { Register } from "./Register";
+import RegistrationForm from "./RegisterCollab";
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
+
+  return (
+    <div className="App">
+      {
+      currentForm === "login" ? 
+        <Login onFormSwitch = {toggleForm} />
+       : 
+        <RegistrationForm onFormSwitch ={toggleForm} />
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Register.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+export const Register = (props) => {
+    const [email,setEmail] = useState('');
+    const [pass,setPass] = useState('');
+    const [name,setName] = useState('');
+
+    const handleSubmit =(e) =>{
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return(
+        <div className="auth-form-container">
+            <h1>Irvins' Online Banking <br></br>Registration</h1>
+            <form className="register-form" onSubmit ={handleSubmit}>
+                <label htmlFor="name">Full Name</label>
+                <input value = {name} name = "name" id="name" placeholder="Your name"/>
+                <label htmlFor="email">Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type ="email" placeholder="Your email" id="email" name="email"/> 
+                <label htmlFor="password">Password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="Your password" id="password" name="password"/>
+                <button type="submit">Register</button>
+            </form>
+            <button className= "link-btn" onClick={()=>props.onFormSwitch('login')}>Already have an account? Login here!</button>
+        </div>
+    )
+}
