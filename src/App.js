fe0dff00e07fd9913c495dac129a1ad67f50b2ef
Ervin Yong <elrsple@hotmail.com>
2023-08-18 15:28:43 +0800

    Newest Collab

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Login } from "./Login";
-// import { Register } from "./Register";
 import RegistrationForm from "./RegisterCollab";
 
 function App() {
